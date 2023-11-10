c4da566a46eed85e0dfa7f3c84512275da26b5ee
lu <knight_lu@lwk.com>
2023-11-10 21:14:52 +0800

    third commit

--- src/main/java/com/exp/git/GitTest.java
+++ src/main/java/com/exp/git/GitTest.java
@@ -2,7 +2,8 @@ package com.exp.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello,git....");
-        System.out.println("hello,git。。。。。。");
+        System.out.println("hello,git1111");
+        System.out.println("hello,git2222");
+        System.out.println("hello,git3333");
     }
 }
