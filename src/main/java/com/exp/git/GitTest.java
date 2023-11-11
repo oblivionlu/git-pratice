acd96da086117838cae938e4db76c0653b9d0185
lu <knight_lu@lwk.com>
2023-11-11 15:36:16 +0800

    hi-fix commit2

--- src/main/java/com/exp/git/GitTest.java
+++ src/main/java/com/exp/git/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("hello,git1111");
         System.out.println("hello,git2222");
         System.out.println("hello,git3333");
-        System.out.println("hello,git4444");
+        System.out.println("hot-fix test");
     }
 }
